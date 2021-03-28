997e5fbbc51477f85030200ab52763d97d018878
SereneSam <Serenebethsam@gmail.com>
2021-03-28 15:36:03 -0500

    Project Done

--- src/numberguess/NumberGuess.java
+++ src/numberguess/NumberGuess.java
@@ -1,11 +1,37 @@
 package numberguess;
 
+import java.util.Scanner;
+
 public class NumberGuess 
 {
 
     public static void main(String[] args) 
     {
-    
+        Scanner in=new Scanner(System.in);
+        Boolean numberfound=false;
+        int min=1, max=10, mid;
+        System.out.println("Think of a number between "+min+" and "+max);
+        while(!numberfound)
+        {
+            mid=(min+max)/2;
+            System.out.println("Is "+mid+" your number?");
+            String answer=in.next().toLowerCase();
+            if(answer.equals("yes"))
+            {
+                System.out.println("We found your number.");
+                break;
+            }
+            System.out.println("Is your number greater than "+ mid+"?");
+            answer=in.next().toLowerCase();
+            if(answer.equals("yes"))
+            {
+                min=mid;
+            }
+            else
+            {
+                max=mid;
+            }
+        }
     }
     
 }
